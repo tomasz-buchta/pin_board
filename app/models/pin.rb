3648202479fa670ca5c52263c383d398bfce32aa
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-05-17 14:07:13 +0200

    Fixed thumbnails and small styles not working

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,10 @@ class Pin < ActiveRecord::Base
   belongs_to :board
 
   validates_presence_of :title
-  has_attached_file :image, :style => {medium: '400x400',thumb: '100x100'}, :default_url => '/images/:style/missing.png'
+  has_attached_file :image,
+                    :styles => {
+                        medium: '400x400',
+                        thumb: '100x100'}, :default_url => '/images/:style/missing.png'
   validates_attachment_presence :image
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
