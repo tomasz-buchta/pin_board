f19485588442773e4acfb8d6e974cebfbb50f673
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-05-17 15:55:48 +0200

    Description for pin is now seeded properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,13 @@ files = Dir.glob("db/seed_images/*.jpg")
 boards = Board.all
 
 boards.each do |board|
-  50.times do |n|
+  30.times do |n|
     p = Pin.new
     p.title = Faker::Commerce.product_name
     p.board_id = board.id
     file_src = File.join Rails.root,files.sample
     p.image = File.new file_src
+    p.description = Faker::Lorem.sentence
     p.save
   end
 end
