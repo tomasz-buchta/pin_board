2f144ac37e6c058c399da68452a72a1e8b811146
Tomasz Buchta <tomasz.buchta@hotmail.com>
2015-06-28 23:14:13 +0200

    Improved mobile version of page
    Changed the images to 300px width so they fit on mobile
    Upgraded the navbar for mobiles

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -9,7 +9,7 @@ class Pin < ActiveRecord::Base
   validates_presence_of :user
   has_attached_file :image,
                     :styles => {
-                        medium: '400x400',
+                        medium: '300x300',
                         thumb: '100x100'}, :default_url => '/images/:style/missing.png'
   validates_attachment_presence :image
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
